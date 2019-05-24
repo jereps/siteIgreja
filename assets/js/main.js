430664ad7c8b59f59a1b3b855eb3efe53b415477
Jeremias Ponciano Da Silva <jeremiason2911@gmail.com>
2019-05-24 20:00:12 -0300

    feito algumas algerações na logica e layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,27 @@
 var breakpoints = {
-    sm: '(max-width: 600px)',
-    md: '(min-width: 601px) and (max-width: 1024px)',
-    lg: '(min-width: 1026px)'
+    xs: '(max-width: 600px)',
+    sm: '(min-width: 601px) and (max-width: 749px)',
+    md: '(min-width: 750px) and (max-width: 979px)',
+    lg: '(min-width: 980px) and (max-width: 1179px)',
+    xl: '(min-width: 1180px) and (max-width: 1235px)',
+    xlm: '(min-width: 1260px) and (max-width: 1400px)',
+    sxl: '(min-width: 1410px) and (max-width: 1490px)',
+    xxl: '(min-width: 1512px)'
 };
 
 
-Object.keys(breakpoints).forEach(function(breakpoint) {
+Object.keys(breakpoints).forEach(function (breakpoint) {
     var mql = window.matchMedia(breakpoints[breakpoint]);
 
     if (mql.matches) {
+        console.log(breakpoint);
         // alert(breakpoint);
         // window.location.reload();
     }
 
-    mql.addListener(function(mql) {
+    mql.addListener(function (mql) {
         if (mql.matches) {
-            // alert(breakpoint);
+            console.log(breakpoint);
             window.location.reload();
         }
     });
