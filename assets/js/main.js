f8c646c4b3a381ae0d1c535ce7c540eb113aa32f
Jeremias Ponciano Da Silva <jeremiason2911@gmail.com>
2019-05-29 18:18:18 -0300

    começado fazer map

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,37 @@ Object.keys(breakpoints).forEach(function (breakpoint) {
         }
     });
 });
+
+var map = L.map(document.getElementById('mapDIV'), {
+    center: [-23.210362, -45.896853],
+    zoom: 12
+});
+
+var basemap = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {});
+
+var basetopo = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/WMTS/tile/1.0.0/USGSTopo/default/default028mm/{z}/{y}/{x}.png', {});
+
+var baserelief = L.tileLayer('https://tile.opentopomap.org/{z}/{x}/{y}.png', {});
+
+basemap.addTo(map);
+
+var thetrail = L.geoJSON(trail, {
+    color: '#800000',
+    weight: 3,
+    dashArray: '12 8 12',
+});
+
+thetrail.bindTooltip('The Valdez-Eagle Trail')
+thetrail.addTo(map);
+
+var baselayers = {
+    'Shared Relief': basemap,
+    'National Map topo': basetopo
+};
+
+var overlays = {
+    'The Trail': thetrail
+};
+
+L.control.layers(baselayers, overlays).addTo(map);
+
